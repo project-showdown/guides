b7c40f0e39247bcc7c4b46add169a2020a9fa84b
RohanGahlot <167160683+RohanGahlot@users.noreply.github.com>
2024-08-14 03:31:37 -0600

    Added all main files

---
+++ public/script.js
@@ -0,0 +1,11 @@
+function switchTab(tabName) {
+    // Hide both sections
+    document.getElementById('userGuide').style.display = 'none';
+    document.getElementById('configGuide').style.display = 'none';
+
+    // Show the selected section
+    document.getElementById(tabName).style.display = 'block';
+}
+
+// Default to showing the User Guide
+document.getElementById('userGuideTab').click();
